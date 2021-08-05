693c8842d2a5ac448c6e16eabe64b0da1f918c2a
Abdülkerim AKSAK <abdulkerim.aksak@outlook.com>
2021-08-05 12:34:40 +0300

    Create User.java

---
+++ src/main/java/com/tutorial/backend/model/User.java
@@ -0,0 +1,35 @@
+package com.tutorial.backend.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+	
+	public User(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "first_name", nullable = false, length = 50)
+	private String firstName;
+	
+	@Column(name = "last_name", nullable = false, length = 50)
+	private String lastName;
+	
+}
