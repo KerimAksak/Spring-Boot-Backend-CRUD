7f327df018a7dd11b970dae16ab90543e33e0b38
Abdülkerim AKSAK <abdulkerim.aksak@outlook.com>
2021-08-05 14:11:36 +0300

    Create UserRepository.java

---
+++ src/main/java/com/tutorial/backend/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.tutorial.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tutorial.backend.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
