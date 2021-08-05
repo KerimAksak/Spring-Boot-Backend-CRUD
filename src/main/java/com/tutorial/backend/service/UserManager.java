01a86cfeb1b0b5bf47401906f26240b386a1e59f
Abdülkerim AKSAK <abdulkerim.aksak@outlook.com>
2021-08-05 20:33:21 +0300

    Created business layer

---
+++ src/main/java/com/tutorial/backend/service/UserManager.java
@@ -0,0 +1,10 @@
+package com.tutorial.backend.service;
+
+public class UserManager implements UserService {
+	
+	@Override
+	public String hello() {
+		return "Hello World!";
+	}
+
+}
---
+++ src/main/java/com/tutorial/backend/service/UserService.java
@@ -0,0 +1,8 @@
+package com.tutorial.backend.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService {
+	String hello();
+}
