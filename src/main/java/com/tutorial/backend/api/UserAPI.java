54d4d239715d9101344df51e01fecc3ece19d46b
Abdülkerim AKSAK <abdulkerim.aksak@outlook.com>
2021-08-07 22:44:31 +0300

    Created createUser API

--- src/main/java/com/tutorial/backend/api/UserAPI.java
+++ src/main/java/com/tutorial/backend/api/UserAPI.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tutorial.backend.dto.UserViewDTO;
@@ -27,6 +29,11 @@ public class UserAPI {
 	public ResponseEntity<UserViewDTO> getUserById(@PathVariable Long id) throws NotFoundException {
 		final UserViewDTO user = userService.getUserById(id);
 		return ResponseEntity.ok(user);
+	}
+	
+	@PostMapping(path="/v1/createuser")
+	public ResponseEntity<?> createUser(@ResponseBody UserCreateDTO userCreateDTO){
+		return null;
 	}
 
 }
