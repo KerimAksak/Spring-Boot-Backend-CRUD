fc03007244d584b6de6fb4bd244645b1cfe138bf
Abdülkerim AKSAK <abdulkerim.aksak@outlook.com>
2021-08-07 23:10:54 +0300

    Created createUser method in business layer

--- src/main/java/com/tutorial/backend/service/UserManager.java
+++ src/main/java/com/tutorial/backend/service/UserManager.java
@@ -3,6 +3,7 @@ package com.tutorial.backend.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.tutorial.backend.dto.UserCreateDTO;
 import com.tutorial.backend.dto.UserViewDTO;
 import com.tutorial.backend.model.User;
 import com.tutorial.backend.repository.UserRepository;
@@ -23,6 +24,13 @@ public class UserManager implements UserService {
 	public UserViewDTO getUserById(Long id) throws NotFoundException {
 		final User user = userRepository.findById(id)
 				.orElseThrow(() -> new NotFoundException("Not Found"));
+		return UserViewDTO.of(user);
+	}
+
+	@Override
+	public UserViewDTO createUser(UserCreateDTO userCreateDTO) {
+		final User user = userRepository
+				.save(new User(userCreateDTO.getFirstName(), userCreateDTO.getLastName()));
 		return UserViewDTO.of(user);
 	}
 	
--- src/main/java/com/tutorial/backend/service/UserService.java
+++ src/main/java/com/tutorial/backend/service/UserService.java
@@ -1,9 +1,12 @@
 package com.tutorial.backend.service;
 
+import com.tutorial.backend.dto.UserCreateDTO;
 import com.tutorial.backend.dto.UserViewDTO;
 
 import javassist.NotFoundException;
 
 public interface UserService {
 	UserViewDTO getUserById(Long id) throws NotFoundException;
+	
+	UserViewDTO createUser(UserCreateDTO userCreateDTO);
 }
