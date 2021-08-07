9a3cfd836b2286f26b7a9b384bb5817f4de3e1df
Abdülkerim AKSAK <abdulkerim.aksak@outlook.com>
2021-08-07 12:19:15 +0300

    Created getUserById endpoint to API layer

--- src/main/java/com/tutorial/backend/api/UserAPI.java
+++ src/main/java/com/tutorial/backend/api/UserAPI.java
@@ -1,11 +1,16 @@
 package com.tutorial.backend.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tutorial.backend.dto.UserViewDTO;
 import com.tutorial.backend.service.UserService;
+
+import javassist.NotFoundException;
 
 @RestController
 @RequestMapping(path = "/api")
@@ -18,9 +23,10 @@ public class UserAPI {
 		this.userService = userService;
 	}
 	
-	@GetMapping(path ="/v1/hello")
-	public String hello() {
-		return userService.hello();
+	@GetMapping(path ="/v1/getUserById/{id}")
+	public ResponseEntity<UserViewDTO> getUserById(@PathVariable Long id) throws NotFoundException {
+		final UserViewDTO user = userService.getUserById(id);
+		return ResponseEntity.ok(user);
 	}
 
 }
