81fbdaf81157322024fbcc44fdd75f5f97b58c54
Abdülkerim AKSAK <abdulkerim.aksak@outlook.com>
2021-08-07 12:24:21 +0300

    Package name changed

--- src/main/java/com/tutorial/backend/BackendApplication.java
+++ src/main/java/com/tutorial/backend/BackendApplication.java
@@ -22,7 +22,7 @@ public class BackendApplication {
     public Docket api() { 
         return new Docket(DocumentationType.SWAGGER_2)  
           .select()                                  
-          .apis(RequestHandlerSelectors.basePackage("springTutorial.northwind"))                                       
+          .apis(RequestHandlerSelectors.basePackage("com.tutorial.backend"))                                       
           .build();                                           
     }
 
