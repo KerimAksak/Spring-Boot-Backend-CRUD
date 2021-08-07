476f2a5840a94198e54e15bdd2cbe307e9022d4e
Abdülkerim AKSAK <abdulkerim.aksak@outlook.com>
2021-08-07 23:04:55 +0300

    Update GenericResponse.java

--- src/main/java/com/tutorial/backend/shared/GenericResponse.java
+++ src/main/java/com/tutorial/backend/shared/GenericResponse.java
@@ -1,5 +1,16 @@
 package com.tutorial.backend.shared;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public final class GenericResponse {
+	
+	private String message;
+	
+	
 
 }
