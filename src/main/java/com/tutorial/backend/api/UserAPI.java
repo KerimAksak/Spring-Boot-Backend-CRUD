aa1f3f28f3bd5728ed66490ccdd8bbd0d41933a1
Abdülkerim AKSAK <abdulkerim.aksak@outlook.com>
2021-08-08 20:27:34 +0300

    Created deleteUser API

--- src/main/java/com/tutorial/backend/api/UserAPI.java
+++ src/main/java/com/tutorial/backend/api/UserAPI.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,8 +56,14 @@ public class UserAPI {
 
 	@PutMapping(path = "/v1/userUpdate/{id}")
 	public ResponseEntity<UserViewDTO> userUpdate(@PathVariable("id") Long id, 
-			@RequestBody UserUpdateDTO UserUpdateDTO){
+			@RequestBody UserUpdateDTO UserUpdateDTO) throws NotFoundException{
 		return ResponseEntity.ok(userService.updateUser(id, UserUpdateDTO));
+	}
+	
+	@DeleteMapping(path = "/v1/deleteUser/{id}")
+	public ResponseEntity<?> deleteUser(@PathVariable("id") Long id){
+		userService.deleteUser(id);
+		return ResponseEntity.ok(new GenericResponse("User Deleted!"));
 	}
 
 }
