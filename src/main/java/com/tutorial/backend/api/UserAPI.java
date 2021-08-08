b69c21b8e0221ecee3e25627f6d68d3e1cb77767
Abdülkerim AKSAK <abdulkerim.aksak@outlook.com>
2021-08-08 16:23:32 +0300

    Added annotations to API layer

--- src/main/java/com/tutorial/backend/api/UserAPI.java
+++ src/main/java/com/tutorial/backend/api/UserAPI.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,18 +32,21 @@ public class UserAPI {
 	}
 	
 	@GetMapping(path ="/v1/getUserById/{id}")
+	@Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
 	public ResponseEntity<UserViewDTO> getUserById(@PathVariable Long id) throws NotFoundException {
 		final UserViewDTO user = userService.getUserById(id);
 		return ResponseEntity.ok(user);
 	}
 	
 	@PostMapping(path="/v1/createuser")
+	@Transactional
 	public ResponseEntity<?> createUser(@RequestBody UserCreateDTO userCreateDTO){
 		userService.createUser(userCreateDTO);	
 		return ResponseEntity.ok(new GenericResponse("Registration Successful"));
 	}
 		
 	@GetMapping(path = "/v1/getAllUsers")
+	@Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
 	public ResponseEntity<List<UserViewDTO>> getAllUsers(){
 		return ResponseEntity.ok(userService.getAllUsers());
 	}
