077d810571221ff2ac9286e6accbd6e4021c6458
Abdülkerim AKSAK <abdulkerim.aksak@outlook.com>
2021-08-09 12:19:08 +0300

    Added validation properties for DTO class..

--- src/main/java/com/tutorial/backend/dto/UserCreateDTO.java
+++ src/main/java/com/tutorial/backend/dto/UserCreateDTO.java
@@ -1,5 +1,6 @@
 package com.tutorial.backend.dto;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import lombok.Data;
@@ -7,9 +8,14 @@ import lombok.Data;
 @Data
 public final class UserCreateDTO {
 	
-	@Size(min = 4, max = 32, message = "First Name should be length min 4-max 32.")
+	@NotNull(message = "{backend.validation.constraints.firstname.NotNull.message}")
+	@Size(min = 4, max = 32, 
+			message = "{backend.validation.constraints.firstname.Size.message}")
 	private String firstName;
 	
+	@NotNull(message = "{backend.validation.constraints.lastname.NotNull.message}")
+	@Size(min = 4, max = 32, 
+			message = "{backend.validation.constraints.lastname.Size.message}")
 	private String lastName;
 	
 	
--- src/main/java/com/tutorial/backend/dto/UserUpdateDTO.java
+++ src/main/java/com/tutorial/backend/dto/UserUpdateDTO.java
@@ -1,12 +1,21 @@
 package com.tutorial.backend.dto;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
 public class UserUpdateDTO {
 	
+	@NotNull(message = "{backend.validation.constraints.firstname.NotNull.message}")
+	@Size(min = 4, max = 32, 
+			message = "{backend.validation.constraints.firstname.Size.message}")
 	private String firstName;
 	
+	@NotNull(message = "{backend.validation.constraints.firstname.NotNull.message}")
+	@Size(min = 4, max = 32, 
+			message = "{backend.validation.constraints.firstname.Size.message}")
 	private String lastName;
 
 }
