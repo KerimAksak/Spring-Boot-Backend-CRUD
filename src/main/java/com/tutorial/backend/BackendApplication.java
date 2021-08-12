fc9cb3de6b5c58a2b1cc1c0ece238c302a2da707
Abdülkerim AKSAK <abdulkerim.aksak@outlook.com>
2021-08-12 22:32:32 +0300

    For H2D, a ready user is created at every boot.

--- src/main/java/com/tutorial/backend/BackendApplication.java
+++ src/main/java/com/tutorial/backend/BackendApplication.java
@@ -1,9 +1,12 @@
 package com.tutorial.backend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.tutorial.backend.dto.UserCreateDTO;
+import com.tutorial.backend.service.UserService;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -16,6 +19,17 @@ public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner createInitialUser(UserService userService) {
+		return(args) -> {
+			UserCreateDTO user = new UserCreateDTO();
+			user.setUserName("kerim.aksak");
+			user.setFirstName("Kerim");
+			user.setLastName("AKSAK");
+			userService.createUser(user);
+		};
 	}
 	
 	@Bean
